0f4d0df624f6cb740ac4a4bffddbca0492dee530
Nicholas Liu <51180693+liunicholas@users.noreply.github.com>
2023-07-03 17:34:41 -0700

    made jack sparrow beautiful

--- jack_sparrow.py
+++ jack_sparrow.py
@@ -1,5 +1,3 @@
-import cv2
-import pytesseract
 import time
 from selenium import webdriver
 import trading_clock as tc
@@ -15,34 +13,45 @@ options = webdriver.ChromeOptions()
 options.add_argument("window-size=1440, 751")
 options.add_argument("--profile-directory=Profile 10")
 options.add_argument("--headless")
-# options.add_argument("user-data-dir=/Users/jameshou/Library/Application Support/Google/Chrome/User Data")
-driver = webdriver.Chrome(options=options)
-driver.maximize_window()
-# driver.get('https://www.tradingview.com/chart/?symbol=EIGHTCAP%3ASPX500')
-# driver.get("https://www.tradingview.com/symbols/CBOE-SPX/")
-driver.get("https://www.tradingview.com/symbols/EIGHTCAP-SPX500/")
 
-def scrape_day():
-    date = tc.get_today().date()
-    start = tc.localize(datetime.datetime.combine(date, datetime.time(9, 35, 0)))
-    date_range = pd.date_range(start, start + datetime.timedelta(hours=6, minutes=25), freq=datetime.timedelta(minutes=5))
+spxCFDdriver = webdriver.Chrome(options=options)
+spxCFDdriver.get("https://www.tradingview.com/symbols/EIGHTCAP-SPX500/")
 
-    open = np.empty((len(date_range)))
-    open[:] = np.nan
-    high = np.empty((len(date_range)))
-    high[:] = -1
-    low = np.empty((len(date_range)))
-    low[:] = float("inf")
-    close = np.empty((len(date_range)))
-    close[:] = np.nan
+ndqCFDdriver = webdriver.Chrome(options=options)
+ndqCFDdriver.get("https://www.tradingview.com/symbols/EIGHTCAP-NDQ100/")
+
+spxFUTURESdriver = webdriver.Chrome(options=options)
+spxFUTURESdriver.get("https://www.tradingview.com/symbols/CME_MINI-ES1!/")
+
+ndqFUTURESdriver = webdriver.Chrome(options=options)
+ndqFUTURESdriver.get("https://www.tradingview.com/symbols/CME_MINI-NQ1!/")
+
+class JackSparrow():
+    def __init__(self, name, driver):
+        self.name = name
+        self.driver = driver
+        
+        date = tc.get_today().date()
+        start = tc.localize(datetime.datetime.combine(date, datetime.time(9, 35, 0)))
+        date_range = pd.date_range(start, start + datetime.timedelta(hours=6, minutes=25), freq=datetime.timedelta(minutes=5))
+
+        open = np.empty((len(date_range)))
+        open[:] = np.nan
+        high = np.empty((len(date_range)))
+        high[:] = -1
+        low = np.empty((len(date_range)))
+        low[:] = float("inf")
+        close = np.empty((len(date_range)))
+        close[:] = np.nan
+
+        self.day_df = pd.DataFrame(
+            {'Date': date_range, 'Open': open, "High": high, "Low": low, "Close": close}).set_index('Date')
+        # self.day_df = self.day_df.set_index('Date')
     
-    day_df = pd.DataFrame({'Date': date_range, 'Open':open, "High":high, "Low":low, "Close":close})
-    day_df = day_df.set_index('Date')
-    while tc.is_market_open(security_type="ETF", current_datetime=tc.get_today()):
-        t1 = time.time()
+    def scrape_moment(self):
         while True:
             try:
-                current_price = float(driver.execute_script("""
+                current_price = float(self.driver.execute_script("""
                 return Array.prototype.slice.call(document.getElementsByClassName("last-JWoJqCpY"));
                 """)[0].text)
                 # print(current_price)
@@ -52,25 +61,39 @@ def scrape_day():
                 break
 
         current_datetime = tc.get_today()
-        if (current_datetime.minute)%5 == 0 and current_datetime.second == 0:
-            day_df["Open"][[tc.next_five_minute(tc.get_today())]] = current_price
-            day_df["High"][[tc.next_five_minute(tc.get_today())]] = current_price
-            day_df["Low"][[tc.next_five_minute(tc.get_today())]] = current_price
+        if (current_datetime.minute) % 5 == 0 and current_datetime.second == 0:
+            self.day_df["Open"][[tc.next_five_minute(
+                tc.get_today())]] = current_price
+            self.day_df["High"][[tc.next_five_minute(
+                tc.get_today())]] = current_price
+            self.day_df["Low"][[tc.next_five_minute(
+                tc.get_today())]] = current_price
 
-        # if (current_datetime.minute+1)%5 == 0 and abs(current_datetime.second-57) <= 2:
-        #     day_df["Close"][[tc.next_five_minute(tc.get_today())]] = current_price
+        if current_price > self.day_df["High"][[tc.next_five_minute(tc.get_today())]].item():
+            self.day_df["High"][[tc.next_five_minute(
+                tc.get_today())]] = current_price
+        if current_price < self.day_df["Low"][[tc.next_five_minute(tc.get_today())]].item():
+            self.day_df["Low"][[tc.next_five_minute(
+                tc.get_today())]] = current_price
 
-        if current_price > day_df["High"][[tc.next_five_minute(tc.get_today())]].item():
-            day_df["High"][[tc.next_five_minute(tc.get_today())]] = current_price
-        if current_price < day_df["Low"][[tc.next_five_minute(tc.get_today())]].item():
-            day_df["Low"][[tc.next_five_minute(tc.get_today())]] = current_price
+        self.day_df["Close"][[tc.next_five_minute(tc.get_today())]] = current_price
+
+        # self.day_df.dropna().to_csv(f"data/{self.name}_{tc.get_today().date()}.csv")
+        self.day_df.to_csv(f"data/{self.name}_{tc.get_today().date()}.csv")
+
+def scrape_day():
+
+    spxCFDscraper = JackSparrow("spxCFD", spxCFDdriver)
+    ndqCFDscraper = JackSparrow("ndqCFD", ndqCFDdriver)
+    spxFUTURESscraper = JackSparrow("spxFUTURES", spxFUTURESdriver)
+    ndqFUTURESscraper = JackSparrow("ndqFUTURES", ndqFUTURESdriver)
+    
+    while tc.is_market_open(security_type="ETF", current_datetime=tc.get_today()):
+        spxCFDscraper.scrape_moment()
+        ndqCFDscraper.scrape_moment()
+        spxFUTURESscraper.scrape_moment()
+        ndqFUTURESscraper.scrape_moment()
         
-        day_df["Close"][[tc.next_five_minute(tc.get_today())]] = current_price
-
-        # day_df.dropna().to_csv(f"data/{tc.get_today().date()}.csv")
-        day_df.to_csv(f"data/spx_{tc.get_today().date()}.csv")
-        t2 = time.time()
-        print(t2-t1)
 def main():
     LIVE = True
     last_known_minute = -1
@@ -85,7 +108,6 @@ def main():
 
             else:
                 print("MARKET CLOSED")
-
 
 if __name__ == "__main__":
     main()
